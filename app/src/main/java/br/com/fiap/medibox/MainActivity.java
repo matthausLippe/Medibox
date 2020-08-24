810d42ee51962f78fd1a14331468786ea08beede
Caique Seren <caiqueseren@gmail.com>
2020-08-24 14:38:15 -0300

    Menu chamando os fragments

--- app/src/main/java/br/com/fiap/medibox/MainActivity.java
+++ app/src/main/java/br/com/fiap/medibox/MainActivity.java
@@ -1,14 +1,77 @@
 package br.com.fiap.medibox;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new TimeLineFragment()).commit();
+            navigationView.setCheckedItem(R.id.timeline_item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.medicacao_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MedicacoesFragment()).commit();
+                break;
+
+            case R.id.paciente_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ResidentesFragment()).commit();
+                break;
+
+            case R.id.timeline_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TimeLineFragment()).commit();
+                break;
+
+            case R.id.conf_item:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ConfiguracoesFragment()).commit();
+                break;
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
