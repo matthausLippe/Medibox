b7c9599d472ddfeabcc04054f6d9c283d9845ad6
Caique Seren <caiqueseren@gmail.com>
2020-08-29 21:08:06 -0300

    Login

---
+++ app/src/main/java/br/com/fiap/medibox/LoginActivity.java
@@ -0,0 +1,27 @@
+package br.com.fiap.medibox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login_bt = (Button) findViewById(R.id.bt_login);
+
+        login_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
