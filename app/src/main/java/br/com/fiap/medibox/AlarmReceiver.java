e72a56bceb11909f13236d4dbaba34f2e2c35cb4
Caique Seren <caiqueseren@gmail.com>
2020-10-12 18:50:12 -0300

    notificação alerta funcionando

--- app/src/main/java/br/com/fiap/medibox/AlarmReceiver.java
+++ app/src/main/java/br/com/fiap/medibox/AlarmReceiver.java
@@ -1,4 +1,56 @@
 package br.com.fiap.medibox;
 
-public class AlarmReceiver {
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import br.com.fiap.medibox.view.MainActivity;
+
+import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
+
+public class AlarmReceiver extends BroadcastReceiver{
+    private static final String CHANNEL_ID = "channel1";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(100, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+
+        Notification notification = builder.setContentTitle("Medibox - Alarme")
+                .setContentText("Horário de medicação do Residente")
+                .setTicker("Atenção!")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setChannelId(CHANNEL_ID)
+                .setContentIntent(pendingIntent).build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+        }
+        notificationManager.notify(0, notification);
+    }
+
 }
--- app/src/main/java/br/com/fiap/medibox/view/MainActivity.java
+++ app/src/main/java/br/com/fiap/medibox/view/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.fiap.medibox.view;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +21,8 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.navigation.NavigationView;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.fiap.medibox.AlarmReceiver;
@@ -56,9 +60,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         //alarm
-
+        createNotificationChannel();
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        //criaAlarmes(alarmManager);
+        criaAlarmes(alarmManager);
 
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -68,12 +72,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void criaAlarmes(AlarmManager alarmManager) {
-        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
-        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent notificationIntent = new Intent("action.DISPLAY_NOTIFICATION");
+
 
         //chama query que traz o medicamento/paciente
-        List<TimeLineModel> lista = timeLineViewModel.getLitNotification(new Date(new java.util.Date().getTime()));
-        String nomeResidente = lista.get(0).getResidenteModel().getNomeResidente();
+        //List<TimeLineModel> lista = timeLineViewModel.getLitNotification(new Date(new java.util.Date().getTime()));
+        List<TimeLineModel> lista =new ArrayList<TimeLineModel>();
+        TimeLineModel l = new TimeLineModel();
+
+        Calendar date = Calendar.getInstance();
+        Date d = new Date(date.getTimeInMillis() + 30000);
+        l.setDataHoraMedicacao(d);
+        l.setIdCliente(1);
+        l.setIdTimeLine(1);
+        l.setIdResidenteMedicamento(1);
+        lista.add(l);
+
+        for(int i = 0; i<lista.size(); i++){
+            PendingIntent broadcast = PendingIntent.getBroadcast(this, i, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, lista.get(i).getDataHoraMedicacao().getTime(), broadcast);
+
+        }
 
     }
 
@@ -117,6 +136,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         return true;
+    }
+
+    private void createNotificationChannel() {
+        CharSequence name = "channel1";
+        String description = "Canal de notificação";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel channel = new NotificationChannel("channel1",name,importance);
+        channel.setDescription(description);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
     @Override
